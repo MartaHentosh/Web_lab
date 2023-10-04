381896006b10e1d8c6a740b5c5ea3a8047653471
MartaHentosh <marta25gentosh@gmail.com>
2023-10-04 22:18:31 +0300

    adding correctly working lab3

--- js/dom_util.js
+++ js/dom_util.js
@@ -1,55 +1,66 @@
-// Функція для перемикання сортування
+let initialOrder = []; // Зберігатимемо початковий порядок елементів
+
 export function toggleSort() {
-  const itemsContainer = document.getElementById('items_container');
-  const sortSwitch = document.getElementById('sort_switch');
+    const itemsContainer = document.getElementById('items_container');
+    const sortSwitch = document.getElementById('sort_switch');
+    let itemBlocks = Array.from(itemsContainer.querySelectorAll('.item__block'));
 
-  // Отримуємо всі блоки товарів
-  const itemBlocks = Array.from(itemsContainer.querySelectorAll('.item__block'));
+    // Якщо це перше включення перемикача, збережемо початковий порядок елементів
+    if (initialOrder.length === 0) {
+        initialOrder = [...itemBlocks];
+    }
 
-  // Сортуємо товари за ціною (від найдорожчого до найдешевшого або навпаки)
-  if (sortSwitch.checked) {
-      itemBlocks.sort((a, b) => {
-          const priceA = parseFloat(a.querySelector('.item__price').textContent.replace('$', ''));
-          const priceB = parseFloat(b.querySelector('.item__price').textContent.replace('$', ''));
-          return priceA - priceB;
-      });
-  } else {
-      itemBlocks.sort((a, b) => {
-          const priceA = parseFloat(a.querySelector('.item__price').textContent.replace('$', ''));
-          const priceB = parseFloat(b.querySelector('.item__price').textContent.replace('$', ''));
-          return priceB - priceA;
-      });
-  }
+    // Виконуємо сортування або відновлюємо початковий порядок
+    if (sortSwitch.checked) {
+        itemBlocks.sort((a, b) => {
+            const priceA = parseFloat(a.querySelector('.item__price').textContent.replace('$', ''));
+            const priceB = parseFloat(b.querySelector('.item__price').textContent.replace('$', ''));
+            return priceB - priceA;
+        });
+    } else {
+        itemBlocks = [...initialOrder];
+    }
 
-  // Переносимо відсортовані товари назад у контейнер
-  itemBlocks.forEach((item) => itemsContainer.appendChild(item));
+    // Видаляємо всі елементи з контейнера
+    while (itemsContainer.firstChild) {
+        itemsContainer.removeChild(itemsContainer.firstChild);
+    }
+
+    // Додаємо елементи в контейнер у відсортованому порядку або початковому порядку
+    itemBlocks.forEach((item) => itemsContainer.appendChild(item));
 }
 
-// Функція для пошуку товарів за назвою
+
+
+
+
+
+
+
+
 export function searchItems() {
-  const searchInput = document.getElementById('search_input').value.toLowerCase();
-  const itemBlocks = Array.from(document.querySelectorAll('.item__block'));
+    const searchInput = document.getElementById('search_input').value.toLowerCase();
+    const itemBlocks = Array.from(document.querySelectorAll('.item__block'));
 
-  itemBlocks.forEach((item) => {
-      const itemName = item.querySelector('.item__paragraph').textContent.toLowerCase();
-      if (itemName.includes(searchInput)) {
-          item.style.display = 'block';
-      } else {
-          item.style.display = 'none';
-      }
-  });
+    itemBlocks.forEach((item) => {
+        const itemName = item.querySelector('.item__paragraph').textContent.toLowerCase();
+        if (itemName.includes(searchInput)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        }
+    });
 }
 
-// Функція для підрахунку загальних витрат
 export function countTotalExpenses() {
-  const itemBlocks = document.querySelectorAll('.item__block');
-  let totalExpenses = 0;
+    const itemBlocks = document.querySelectorAll('.item__block');
+    let totalExpenses = 0;
 
-  itemBlocks.forEach((item) => {
-      const price = parseFloat(item.querySelector('.item__price').textContent.replace('$', ''));
-      totalExpenses += price;
-  });
+    itemBlocks.forEach((item) => {
+        const price = parseFloat(item.querySelector('.item__price').textContent.replace('$', ''));
+        totalExpenses += price;
+    });
 
-  const countResultLabel = document.getElementById('count_result_label');
-  countResultLabel.textContent = `Total expenses: $${totalExpenses}`;
+    const countResultLabel = document.getElementById('count_result_label');
+    countResultLabel.textContent = `Total expenses: $${totalExpenses}`;
 }
